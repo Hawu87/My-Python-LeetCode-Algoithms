6f8a12125b367531ee545f444292e071397af109
Hawulethu Ndlovu <117247847+Hawu87@users.noreply.github.com>
2023-06-07 11:01:43 -0600

    RomanToInteger.py

--- RomanToInteger.py
+++ RomanToInteger.py
@@ -1,5 +1,4 @@
-def roman(s):
-	class Solution:
+class Solution:
     def romanToInt(s: str) -> int:
     # Dictionary of roman numerals
     roman_map = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
