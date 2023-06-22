b125c52845385950187699e290aae28961270918
Hawulethu Ndlovu <117247847+Hawu87@users.noreply.github.com>
2023-06-21 21:08:43 -0600

    Add files via upload

---
+++ ProductOfSelfExceptArray.py
@@ -0,0 +1,11 @@
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        temp, output = 1, []
+        for i in range(len(nums)):
+            output.append(temp)
+            temp *= nums[i]
+        temp = 1
+        for i in range(len(nums) - 1, -1, -1):
+            output[i] *= temp
+            temp *= nums[i]
+        return output
