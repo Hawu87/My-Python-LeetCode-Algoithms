dc914dbbe069b381292871976946014d426a3ca4
Hawulethu Ndlovu <117247847+Hawu87@users.noreply.github.com>
2023-06-29 20:31:12 -0600

    isPalindromestring.py

--- isPalindromestring.py
+++ isPalindromestring.py
@@ -1,13 +1,21 @@
-def isPalindrome(self, s):
-        """
-        :type s: str
-        :rtype: bool
-        """
-        s = s.lower()
-        for i in s:
-            if not (i.isalnum()):
-                s = s.replace(i, "")
-        return s == s[::-1]
+def isPalindrome(self, s: str) -> bool:
+        left, right = 0, len(s) - 1
+      
+        while left < right:
+                while (left < right and not self.isAlNum(s[left])):
+                        left += 1
+                while (left < right and not self.isAlNum(s[right])):
+                        right -= 1
+                if s[left].lower() != s[right].lower():
+                        return False
+                    left, right = left + 1, right - 1
+        
+        return True
+    
+def isAlNum(self, s: str) -> bool:
+        return (ord("A") <= ord(s) <= ord("Z") or
+      		ord("a") <= ord(s) <= ord("z") or
+          ord("0") <= ord(s) <= ord("9"))
 
 
 
