c9b567a72a16a66c3d3d07369e8909719e0e351f
Hawulethu Ndlovu <117247847+Hawu87@users.noreply.github.com>
2023-07-26 23:07:07 -0500

    BestTimeToBuyAndSellStock.py

--- BestTimeToBuyAndSellStock.py
+++ BestTimeToBuyAndSellStock.py
@@ -2,12 +2,12 @@ class Solution:
     """
     Input: prices = [7,1,5,3,6,4]
     Output: 5
-    Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5.
+    Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5
     Note that buying on day 2 and selling on day 1 is not allowed because you must buy before you sell.
 
     Input: prices = [7,6,4,3,1]
     Output: 0
-    Explanation: In this case, no transactions are done and the max profit = 0.
+    Explanation: In this case, no transactions are done and the max profit = 0
     """
     def maxProfit(self, prices: List[int]) -> int:
         maximum, l, r = 0, 0, 1
