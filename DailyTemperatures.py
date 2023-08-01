cad73d34a037aa1a07e506d49e76db9a8856a633
Hawulethu Ndlovu <117247847+Hawu87@users.noreply.github.com>
2023-07-31 22:41:27 -0500

    DailyTemperatures.py

---
+++ DailyTemperatures.py
@@ -0,0 +1,16 @@
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        #use a stack to keep track of values
+        #pop value if temperature greater than that found
+        #else append
+        #use a list of tuple to hold index and val
+        stack = []
+        output = [0] * len(temperatures)
+        for index, val in enumerate(temperatures):
+            while stack and val > stack[-1][1]:
+                output[stack[-1][0]] = (index - stack[-1][0])
+                stack.pop()
+            stack.append((index, val))
+        return output
+        
+
